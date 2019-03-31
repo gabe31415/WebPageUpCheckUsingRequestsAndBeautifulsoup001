cee0554732fbc624b056ace3cc02fefff82ecc66
Gabe31415 <scot.gabriel@gmail.com>
2019-03-31 00:26:23 -0500

    Repeat check and email if fail threshold met.

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
+import datetime
 import os
-import requests
-from bs4 import BeautifulSoup
+import time
 
+from bs4 import BeautifulSoup
+from exchangelib import Account, Credentials, Mailbox, Message
+import requests
+
+failCount = 0
+maxFailCount = 3
+nextCheck = 60 # seconds until next check when failCount < maxFailCount
+maxFailCountReachedNextCheck = 1800 # seconds until next check when maxFailCount reached
+
+# pull confidential variables from environment variables
 web01Url01 = os.getenv('web01Url01')
 web01Username01 = os.getenv('web01Username01')
 web01Password01 = os.getenv('web01Password01')
 web01Content01 = os.getenv('web01Content01')
+workEmailAddress = os.getenv('workEmailAddress')
+workEmailPassword = os.getenv('workEmailPassword')
+webPageCheckEmailSubjectLineCustomer02 = os.getenv('webPageCheckEmailSubjectLineCustomer02')
+webPageCheckApp01Customer02 = os.getenv('webPageCheckApp01Customer02')
 
 if web01Url01 == None:
-    print("web01Url01 NOT configured")
+    print('web01Url01 NOT configured')
     exit()
 if web01Username01 == None:
-    print("web01Username01 NOT configured")
+    print('web01Username01 NOT configured')
     exit()
 if web01Password01 == None:
-    print("web01Password01 NOT configured")
+    print('web01Password01 NOT configured')
     exit()
 if web01Content01 == None:
-    print("web01Content01 NOT configured")
+    print('web01Content01 NOT configured')
+    exit()
+if workEmailAddress == None:
+    print('workEmailAddress NOT configured')
+    exit()
+if workEmailPassword == None:
+    print('workEmailPassword NOT configured')
+    exit()
+if webPageCheckEmailSubjectLineCustomer02 == None:
+    print('webPageCheckEmailSubjectLineCustomer02 NOT configured')
+    exit()
+if webPageCheckApp01Customer02 == None:
+    print('webPageCheckApp01Customer02 NOT configured')
     exit()
 
 loginData = {
@@ -34,18 +60,28 @@ loginData = {
 }
 
 def checkFail():
-    # increment fail counter
-    # if fail counter max, run "escalation"
-    
-    # place holder
-    print("checkFailed")
+    print('checkFailed')
+    print('Incrementing fail count')
+    global failCount
+    failCount += 1
+    print('failCount is now', str(failCount))
+    if failCount < maxFailCount:
+        time.sleep(nextCheck)
+    else:
+        print('Sending failure email!')
+        sendEmail()
+        print('Fail threshold reached sleeping for', str(maxFailCountReachedNextCheck), 'seconds!')
+        time.sleep(maxFailCountReachedNextCheck)
+    pageCheck()
+
+
 
 def checkSuccess():
-    # if fail counter not 0, reset to 0, send alert that check is now succeeding
-
-    # place holder
-    print("checkSucceeded")
-    
+    print('checkSucceeded', datetime.datetime.now())
+    failCount = 0
+    print('sleeping for', str(nextCheck), 'seconds.')
+    time.sleep(nextCheck)
+    pageCheck()
 
 def pageCheck():
     with requests.Session() as s:
@@ -65,10 +101,20 @@ def pageCheck():
         else:
             checkFail()
 
-def recheckLogic():
-    # how long to wait between checks, perhaps dependent on current fail count?
-    pass
+def emailConnection():
+    credentials = Credentials(workEmailAddress, workEmailPassword)
+    a = Account(workEmailAddress, credentials=credentials, autodiscover=True)
+    return a
+
+def sendEmail():
+    a = emailConnection()
+    m = Message(
+        account=a,
+        folder=a.sent,
+        subject=webPageCheckEmailSubjectLineCustomer02,
+        body=webPageCheckApp01Customer02+' is DOWN!',
+        to_recipients=[workEmailAddress]
+    )
+    m.send_and_save()
 
 pageCheck()
-
-recheckLogic()
